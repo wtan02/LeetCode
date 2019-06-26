8c7b2bd92e3086c97669705a1bae6be3986746c3
Norman Tan <wtan02@syr.edu>
2019-06-25 21:30:54 -0700

    Add PathSumII

--- src/PathSumII.java
+++ src/PathSumII.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -7,23 +10,40 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-class PathSumII {
+public class PathSumII {
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
-
-    //sum inside the binary tree lead by that node
-    public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    public List<List<Integer>> solution(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        dfs(root, sum, res, path);
+        return res;
     }
 
-    //pathSum starting at that node
-    private int pathSumFrom(TreeNode node, int sum) {
-        if (node == null) return 0;
-        return (node.val == sum ? 1 : 0) + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
+    private void dfs(TreeNode root, int sum, List<List<Integer>> res, List<Integer> path){
+        if(root==null) return;
+        path.add(root.val);
+
+        if(root.left==null && root.right==null ){
+            if(root.val==sum)
+                //Copy constructor, the advantage is that we only copy a list when we add to the final result
+                res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        if(root.left!=null) {
+            dfs(root.left,sum-root.val,res,path);
+            //Remove reference from child layer
+            path.remove(path.size()-1);
+        }
+        if(root.right!=null) {
+            dfs(root.right,sum-root.val,res,path);
+            //Remove reference from child layer
+            path.remove(path.size()-1);
+        }
+
     }
 }
