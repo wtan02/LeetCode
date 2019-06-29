d93f19402975dc68a9feb3627198a348963bc1fe
Norman Tan <wtan02@syr.edu>
2019-06-28 23:34:13 -0700

    fix typo

--- src/tree/rangeSumBST/solution.java
+++ src/tree/rangeSumBST/solution.java
@@ -2,7 +2,7 @@ package tree.rangeSumBST;
 
 import tree.TreeNode;
 
-public class soluiton {
+public class solution {
     private int ans = 0;
 
     public int rangeSumBST(TreeNode root, int L, int R) {
