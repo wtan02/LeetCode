51a02dee58b63cecbff2960c8bd123f60aafbc67
Norman Tan <wtan02@syr.edu>
2019-06-28 23:29:37 -0700

    Add TreeNode

---
+++ src/tree/rangeSumBST/soluiton.java
@@ -0,0 +1,25 @@
+package tree.rangeSumBST;
+
+import tree.TreeNode;
+
+public class soluiton {
+    private int ans = 0;
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        preOrderTraversal(root, L, R);
+        return ans;
+    }
+
+    private void preOrderTraversal(TreeNode node, int L, int R) {
+        if (node == null) return;
+        if (L <= node.val && node.val <= R) {
+            ans += node.val;
+        }
+        if (L < node.val ) {
+            preOrderTraversal(node.left, L, R);
+        }
+        if (node.val < R) {
+            preOrderTraversal(node.right, L, R);
+        }
+    }
+}
