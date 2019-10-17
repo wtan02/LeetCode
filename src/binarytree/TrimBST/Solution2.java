6750540f8d15c6a4adabe8e00ae7760439a8f531
Wenchang Tan <wtan02@Wenchangs-MBP.hsd1.wa.comcast.net>
2019-10-16 20:35:03 -0700

    Add TrimBST

---
+++ src/binarytree/TrimBST/Solution2.java
@@ -0,0 +1,41 @@
+package binarytree.TrimBST;
+
+import binarytree.TreeNode;
+
+public class Solution2 {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return root;
+        }
+        //Find a valid root which is used to return.
+        while (root.val < L || root.val > R) {
+            if (root.val < L) {
+                root = root.right;
+            }
+            if (root.val > R) {
+                root = root.left;
+            }
+        }
+        TreeNode dummy = root;
+        // Iterively remove invalid nodes from left subtree.
+        while (dummy != null) {
+            while (dummy.left != null && dummy.left.val < L) {
+                dummy.left = dummy.left.right;
+                // If the left child is smaller than L, then we just keep the right subtree of it.
+            }
+            dummy = dummy.left;
+            //Iterate to next left subtree
+        }
+        dummy = root;
+        // Iterively remove the invalid nodes from right subtree
+        while (dummy != null) {
+            while (dummy.right != null && dummy.right.val > R) {
+                dummy.right = dummy.right.left;
+                // If the right child is biggrt than R, then we just keep the left subtree of it.
+            }
+            dummy = dummy.right;
+            //Iterate to next right subtree
+        }
+        return root;
+    }
+}
---
+++ src/binarytree/TrimBST/TrimBST.java
@@ -0,0 +1,7 @@
+package binarytree.TrimBST;
+
+import binarytree.TreeNode;
+
+public interface TrimBST {
+    TreeNode trimBST(TreeNode root, int L, int R);
+}
