08cf1b9ef371bfeaf4189fd8a17f4555b46ba220
Norman Tan <wtan02@syr.edu>
2020-03-10 21:35:55 -0700

    balanced binary tree

---
+++ 110.balanced-binary-tree.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return postOrderTraversal(root) != -1;
+    }
+    
+
+    private int postOrderTraversal(TreeNode node) {
+        if (node == null) return 0;
+
+        int left = postOrderTraversal(node.left);
+        if (left != -1) {
+            int right = postOrderTraversal(node.right);
+            if (right != -1) {
+                return Math.abs(left - right) <= 1 ? Math.max(left, right) + 1 : -1;
+            }
+        }
+        return -1;
+    }
+}
+
---
+++ 637.average-of-levels-in-binary-tree.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=637 lang=java
+ *
+ * [637] Average of Levels in Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        
+    }
+
+    private void 
+}
+
