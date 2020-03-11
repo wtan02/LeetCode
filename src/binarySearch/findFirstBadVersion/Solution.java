8569890fb34363c53c6c64dd546d85dcde4cf6e9
Norman Tan <wtan02@syr.edu>
2020-03-10 21:44:45 -0700

    Find first bad version

---
+++ src/binarySearch/findFirstBadVersion/Solution.java
@@ -0,0 +1,23 @@
+package binarySearch.findFirstBadVersion;
+
+public class Solution extends findFirstBadVersion{
+    @Override
+    public int findFirstBadVersion(int n) {
+        int start = 1, end = n;
+
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            }else {
+                start = mid;
+            }
+        }
+
+        if (isBadVersion(start)) {
+            return start;
+        }else {
+            return end;
+        }
+    }
+}
---
+++ src/binarySearch/findFirstBadVersion/findFirstBadVersion.java
@@ -0,0 +1,5 @@
+package binarySearch.findFirstBadVersion;
+
+public interface findFirstBadVersion {
+    public int findFirstBadVersion(int n);
+}
